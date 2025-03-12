007530e0f0a6f2f3952fdc19c2c71080c0a83a9f
livro-mvf <103282478+livro-mvf@users.noreply.github.com>
2025-03-11 23:04:16 -0300

    Update AbstractGrid1D.hpp

--- FVMakerLib/include/FVMaker/Grid/Grid1D/AbstractGrid1D.hpp
+++ FVMakerLib/include/FVMaker/Grid/Grid1D/AbstractGrid1D.hpp
@@ -69,6 +69,8 @@ void AbstractGrid1D<T> :: InitVector () {
 
 template<typename T>
 std::ostream& operator<<(std::ostream& _os, const AbstractGrid1D<T>& _grid1D) {
+
+std::ios_base::fmtflags fflags = _os.flags();
 const int MYSIZE= LSIZE + 5;    
      _os << "\nImpressao do AbstractGrid1D\n";
      PrintLine(_os, MYSIZE);
@@ -87,57 +89,35 @@ const int MYSIZE= LSIZE + 5;
  
 const auto xFace = _grid1D.FaceCoordinate();
 
-//    std::cout << "Aqio   " << xFace[0] << "\n\n";
 
 auto Print = [&](const std::size_t& i)
 {
     std::stringstream ss;
-    ss << std::setw(5) << i;
- //      << std::setw(20) << _grid1D.xCentro_[i];
- //      << std::setw(20) << _grid1D.xFace_[i];
+    ss << std::setw(5) << i
+       << std::scientific 
+       << std::setw(20) << _grid1D.xCentro_[i]
+       << std::setw(20) << _grid1D.xFace_[i]
+       << std::setw(20) << _grid1D.dxCentro_[i]
+       << std::setw(20) << _grid1D.dxFace_[i];
     return ss.str(); 
 };
 
-    std::transform  (   std::execution::par
-                    ,   std::begin(indices)
+    std::transform  (   std::begin(indices)
                     ,   std::end(indices)
                     ,   std::ostream_iterator<std::string>(_os, "\n")
                     ,   Print
                     );
-
-/*
-unsigned volume = 0;
-auto     ptrdxCentro = std::begin(_grid1D.dxCentro_);
-auto     ptrdxFace = std::begin(_grid1D.dxFace_);
-
-auto Print = [&volume, &ptrdxCentro, &ptrdxFace](const auto& _xC, const auto& _xF) {
-    std::stringstream ss;
-    ss << std::setw(5) << volume 
-       << std::scientific 
-       << std::setw(20) << _xC
-       << std::setw(20) << _xF
-       << std::setw(20) << *ptrdxCentro
-       << std::setw(20) << *ptrdxFace;
-    volume++;ptrdxCentro++;ptrdxFace++;
-    return ss.str();    
-};
-
-    std::transform  (   std::begin(_grid1D.xCentro_)
-                    ,   std::end(_grid1D.xCentro_)
-                    ,   std::begin(_grid1D.xFace_)
-                    ,   std::ostream_iterator<std::string>(_os, "\n")
-                    ,   Print
-                    );
-
     
-    _os     << std::setw(5) << volume  << std::scientific << std::setprecision(6)
-            << std::setw(40) << *(std::prev(std::end(_grid1D.xFace_)))
-            << std::setw(20) << *(std::prev(std::end(_grid1D.dxCentro_)))
-            << "\n";
-    
+    _os << std::setw(5) << N
+        << std::scientific
+        << std::setprecision(6)    
+        << std::setw(40) << _grid1D.xFace_[N]
+        << std::setw(20) << _grid1D.dxCentro_[N]
+        << "\n";    
+ 
     _os << std::flush;
     PrintLine(_os, MYSIZE);
-  */   
+    _os.flags(fflags);
     return _os;
 }
 
